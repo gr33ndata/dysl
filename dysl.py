6925f072439027233707c2678912d44e8226c1de
Tarek Amr <gr33ndata@yahoo.com>
2014-02-06 11:33:39 +0200

    Adding initial python code

---
+++ dysl.py
@@ -0,0 +1,43 @@
+import sys
+import codecs
+
+from dyslib.lm import LM
+
+corpora = {
+    'en': 'corpora/en.txt',
+    'es': 'corpora/es.txt',
+    'fr': 'corpora/fr.txt',
+}
+
+def term2ch(text):
+    return [ch for ch in text]
+
+# f = codecs.open('unicode.rst', encoding='utf-8')
+# lm.add_doc(doc_id='apple', doc_terms=term2ch('the tree is full or apples'))
+
+def readfile(filename):
+    f = codecs.open(filename, encoding='utf-8')
+    tokenz = term2ch(f.read())
+    f.close()
+    #print tokenz
+    return tokenz
+
+
+def main():
+    
+    ngram = 2
+    lrpad = ' '
+
+    lm = LM(n=ngram, verbose=True, lpad=lrpad, rpad=lrpad, 
+            smoothing='Laplace', laplace_gama=0.1, 
+            corpus_mix='l')
+    for lang in corpora:
+        lm.add_doc(doc_id=lang, doc_terms=readfile(corpora[lang]))
+
+    intxt = ' '.join(sys.argv[1:])
+    print intxt
+    result = lm.calculate(doc_terms=term2ch(intxt))
+    print result
+
+if __name__ == '__main__':
+    main()
