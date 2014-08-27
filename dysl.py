e7dd6e3e4834f15caf6921d1123a7e9f99ff711d
Tarek Amr <gr33ndata@yahoo.com>
2014-08-27 13:50:17 +0300

    Adding CLI options to dysl.py

--- dysl.py
+++ dysl.py
@@ -1,19 +1,28 @@
 import sys
-import argparse
+from argparse import ArgumentParser
 from dysl.langid import LangID
 
 def main():
 
-    l = LangID(unk=False)
+    parser = ArgumentParser(description='Do you speak London? A library for Natural Language Identification.')
+    parser.add_argument('--unk', choices=['y','n'], default='y', help='Input text to classify')
+    parser.add_argument('input', nargs='*', help='Input text to classify')
+    args = parser.parse_args()
+    #print args
+
+    unk = False if args.unk == 'n' else True
+    l = LangID(unk=unk)
     l.train()
 
-    if len(sys.argv) > 1:
-        text = u' '.join(sys.argv[1:])
+    if len(args.input):
+        text = u' '.join(args.input)
     else:
-        text = u'hello, world'
+        parser.print_help()
+        sys.exit('\nPlease input some text to classify')
 
     lang = l.classify(text)
-    print text, '[ Language:', lang, ']'
+    print 'Input text:', text
+    print 'Language:', lang
     
 
 if __name__ == '__main__':
