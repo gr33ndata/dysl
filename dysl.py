10a9b46702e14bfac4913f3bed1c12a85a3e481c
Tarek Amr <gr33ndata@yahoo.com>
2014-10-27 17:07:24 +0200

    Adding the ability to list all languages available in training data

--- dysl.py
+++ dysl.py
@@ -15,6 +15,7 @@ def main():
 
     parser = ArgumentParser(description='Do you speak London? A library for Natural Language Identification.')
     parser.add_argument('--version', action='store_true', help='Show version')
+    parser.add_argument('--list-langs', action='store_true', help='List supported languages in training data')
     parser.add_argument('--unk', choices=['y','n'], default='y', help='Input text to classify')
     parser.add_argument('--corpus', default='', help='Specify path to custom training-set')
     parser.add_argument('--lang', help='Add training sample for the language specified')
@@ -28,6 +29,11 @@ def main():
 
     if args.version:
         sys.exit(__version__)
+    elif args.list_langs:
+        l = LangID(unk=unk)
+        l.train(root=args.corpus)
+        print 'Languages: [' + '-'.join(l.get_lang_set()) + ']'
+        sys.exit()
     elif args.lang and input_text:
         l = LangID(unk=unk)
         l.train(root=args.corpus)
