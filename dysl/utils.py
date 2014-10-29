1c9fb06fbb8a397f909faa7469508afa9d0be93a
Tarek Amr <gr33ndata@yahoo.com>
2014-10-29 13:50:00 +0200

    Fixing and commenting utils.py

--- dysl/utils.py
+++ dysl/utils.py
@@ -1,6 +1,12 @@
 def decode_input(text_in):
+    """ Decodes `text_in`
+        If text_in is is a string, 
+        then decode it as utf-8 string.
+        If text_in is is a list of strings,
+        then decode each string of it, 
+        then combine them into one outpust string.  
+    """
     
-    return u' '.join(text_in)
     if type(text_in) == list:
         text_out = u' '.join([t.decode('utf-8') for t in text_in])
     else:
